8953470b8c156da391e95f1cfd855b83a39b608d
Ayaan Gouse <ayaangouse1@gmail.com>
2022-05-31 22:26:19 +0530

    changed the url, method defined

--- app.py
+++ app.py
@@ -1,21 +1,18 @@
 from flask import Flask, render_template, request
-import chatbot_backend as cb
+from chatbot_backend import getResponse
 
-# flask app initialisation
 app = Flask(__name__)
 
-# <html> index page initialisation 
+#define app routes
 @app.route("/")
 def index():
     return render_template("index.html")
 
 @app.route("/get")
-def chatbot_response():
-    msg = request.args.get('usermsg', type=str)
-    print(msg)
-    #response_msg = cb.getResponse(msg)      
-    return msg
-    
-#run server
+#function for the bot response
+def get_bot_response():
+    userText = request.args.get('msg')
+    return str(getResponse(userText))
+
 if __name__ == "__main__":
     app.run(debug=True)
